3c9ec635aa493d5fc439730ef6522070b091ea95
Moustafa <mamutahr@umich.edu>
2018-02-28 20:03:29 -0500

    Add power functionality and test

--- test_rpn.py
+++ test_rpn.py
@@ -17,6 +17,9 @@ class TestBasics(unittest.TestCase):
 	def test_divide(self):
 		result = rpn.calculate('3 3 /')
 		self.assertEqual(1,result)
+	def test_power(self):
+		result = rpn.calculate('3 3 ^')
+		self.assertEqual(27,result)
 	def test_toomany(self):
 		with self.assertRaises(TypeError):
 			result = rpn.calculate('1 2 3 +')
