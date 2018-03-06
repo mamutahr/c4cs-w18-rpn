546b4c4889bb0bca8632dae67094034e6b1b1855
Moustafa <mamutahr@umich.edu>
2018-03-05 23:36:01 -0500

    Comment out a test to bring down coverage

--- test_rpn.py
+++ test_rpn.py
@@ -5,9 +5,9 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
 		self.assertEqual(2, result)
-	def test_adds(self):
-		result = rpn.calculate('1 1 + 2 +')
-		self.assertEqual(4,result)
+#	def test_adds(self):
+#		result = rpn.calculate('1 1 + 2 +')
+#		self.assertEqual(4,result)
 	def test_subtract(self):
 		result = rpn.calculate('5 2 -')
 		self.assertEqual(3,result)
