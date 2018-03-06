9f5c52f2e6d2e34d4827c44174f640c4a06a2ee7
Moustafa <mamutahr@umich.edu>
2018-03-05 23:52:42 -0500

    Another comment to change coverage

--- test_rpn.py
+++ test_rpn.py
@@ -5,15 +5,15 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
 		self.assertEqual(2, result)
-#	def test_adds(self):
-#		result = rpn.calculate('1 1 + 2 +')
-#		self.assertEqual(4,result)
+	def test_adds(self):
+		result = rpn.calculate('1 1 + 2 +')
+		self.assertEqual(4,result)
 	def test_subtract(self):
 		result = rpn.calculate('5 2 -')
 		self.assertEqual(3,result)
-	def test_multiply(self):
-		result = rpn.calculate('3 3 *')
-		self.assertEqual(9,result)
+#	def test_multiply(self):
+#		result = rpn.calculate('3 3 *')
+#		self.assertEqual(9,result)
 	def test_divide(self):
 		result = rpn.calculate('3 3 /')
 		self.assertEqual(1,result)
